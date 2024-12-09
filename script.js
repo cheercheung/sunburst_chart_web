d4e56667839997fa085ab7e9529987b4ef8c3c45
cheer cheung <67986574+cheercheung@users.noreply.github.com>
2024-12-09 20:42:46 +0800

    Update script.js
    修改了跨域资源

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         try {
-            const response = await fetch('/generate_chart', {
+            const response = await fetch('http://127.0.0.1:5000/generate_chart', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
